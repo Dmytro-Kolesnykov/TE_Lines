4424178df7e9c773215e3566827e8ec2438ca4aa
Dmytro-Kolesnykov <125923047+Dmytro-Kolesnykov@users.noreply.github.com>
2025-03-13 21:58:00 +0200

    all_done

---
+++ tab_open.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('toggleButton');
+const element = document.getElementById('myElement');
+button.addEventListener('click', () => { element.classList.toggle('is-hidden'); });
+
+const button1 = document.getElementById('toggleButton1');
+const element1 = document.getElementById('myElement1');
+button1.addEventListener('click', () => { element1.classList.toggle('is-hidden'); });
+
+const button2 = document.getElementById('toggleButton2');
+const element2 = document.getElementById('myElement2');
+button2.addEventListener('click', () => { element2.classList.toggle('is-hidden'); });
+
+const button3 = document.getElementById('toggleButton3');
+const element3 = document.getElementById('myElement3');
+button3.addEventListener('click', () => { element3.classList.toggle('is-hidden'); });
+
+const button4 = document.getElementById('toggleButton4');
+const element4 = document.getElementById('myElement4');
+button4.addEventListener('click', () => { element4.classList.toggle('is-hidden'); });
+
+const button5 = document.getElementById('toggleButton5');
+const element5 = document.getElementById('myElement5');
+button5.addEventListener('click', () => { element5.classList.toggle('is-hidden'); });
+
+const button6 = document.getElementById('toggleButton6');
+const element6 = document.getElementById('myElement6');
+button6.addEventListener('click', () => { element6.classList.toggle('is-hidden'); });
+
+const button7 = document.getElementById('toggleButton7');
+const element7 = document.getElementById('myElement7');
+button7.addEventListener('click', () => { element7.classList.toggle('is-hidden'); });
